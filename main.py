1c6f39570f7b3ff65aee6aaf8994a0d1320f7b1b
pwebtechnology <Usalpint74@proton.me>
2024-07-01 11:34:14 +0300

    cookies debugging

--- main.py
+++ main.py
@@ -253,10 +253,12 @@ def login():
 def refresh():
     try:
         refresh_token = request.cookies.get('refresh_token_cookie')
+        print("here is refresh token:", refresh_token)
         if not refresh_token:
             return jsonify({'error': 'Refresh token is missing'}), 403
 
         decoded_token = decode_token(refresh_token)
+        print(decoded_token)
         identity = decoded_token.get('sub')
 
         if not identity:
